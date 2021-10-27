42d5fab3727e17041946b2064777fff16693f521
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2021-10-27 13:51:42 -0600

    Setting up routes and pages

---
+++ bfdPages/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.urls import path
+
+#This is accessing the views methods/functions we wrote
+from .views import indexPageView
+from .views import aboutPageView
+from .views import productPageView
+
+#Create the paths
+
+urlpatterns = [
+    path('',indexPageView, name="Home"),
+    path('about/',aboutPageView, name="About"),
+    path('products/',productPageView, name="Products")
+
+]
+
--- bfdPages/views.py
+++ bfdPages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def indexPageView(request):
+    return HttpResponse("Home Page")
+
+def aboutPageView(request):
+    return HttpResponse("About Page")
+
+def productPageView(request):
+    return HttpResponse("Product list page")
