56e48b6fe79d99f59aaa789cfaad89fc8abd6db7
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2021-10-27 12:59:37 -0600

    Update asgi.py

--- BFD/BFD_sub/asgi.py
+++ BFD/BFD_sub/asgi.py
@@ -14,3 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BFD.settings')
 
 application = get_asgi_application()
+#this is just comment to test
