26a2aa3f5d915d451777514b99e4835902eef5f4
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2021-10-27 13:06:12 -0600

    Update asgi.py
    my comment! (Zac)

--- BFD/BFD_sub/asgi.py
+++ BFD/BFD_sub/asgi.py
@@ -14,4 +14,5 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BFD.settings')
 
 application = get_asgi_application()
-#this is just comment to test
+# this is just comment to test
+# this is Zac's Comment
