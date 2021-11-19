9728df295e603e3f2a96aca4dc457853664e09f8
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2021-11-19 11:15:20 -0700

    Update settings.py

--- BFD_sub/settings.py
+++ BFD_sub/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+#This is a test comment for the purpose of showing James/Zac
 from pathlib import Path
 import os
 
