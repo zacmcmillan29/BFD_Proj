2446b41b06701b3e7e8ce9d3a9f0e12619ec14bc
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2021-11-19 14:29:36 -0700

    orderpageupdate

--- bfdPages/views.py
+++ bfdPages/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def indexPageView(request):
     return HttpResponse("Home Page")
+
 
 def aboutPageView(request):
     return HttpResponse("About Page")
 
+
 def productPageView(request):
     return HttpResponse("Product list page")
 
+
 def orderPageView(request):
-    return render(request, 'orderpages/BFD_OrderFormcopy.html')
+    return render(request, 'createOrder/createOrder.html')
