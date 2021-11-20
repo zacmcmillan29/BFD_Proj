e42ce39a10a485fca157de756ded3afa583e0fcc
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2021-11-19 17:21:24 -0700

    this is for FORMS

---
+++ TOKNOW.py
@@ -0,0 +1,17 @@
+################## FORMS and where to find them #####################
+# 1. trackOrders
+##### 22.7 = base FORMS ######
+# findTrackOrdersPageView
+# searchTrackOrdersPageView
+##### 22.9 = extra FORMS ######
+# addOrderPageView?
+# storeOrderPageView?
+# updating and deleting/fulfilling (:
+
+##### 22.7 = base FORMS ######
+# 2. createOrders
+# findCreateOrderPageView
+# searchCreateOrderPageView
+##### 22.9 = extra FORMS ######
+# addOrderPageView
+# storeOrderPageView
